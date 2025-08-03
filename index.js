1eaf8e0b6b2333ef6847936dd8abdeaac9eea8ee
TarunPalekar <167878468+TarunPalekar@users.noreply.github.com>
2025-08-04 00:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"quicklearnsproject.vercel.app/",
+		origin:"https://quicklearnsproject.vercel.app/",
 		credentials:true,
 	})
 )
